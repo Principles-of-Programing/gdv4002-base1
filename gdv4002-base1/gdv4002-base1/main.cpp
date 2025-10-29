e7225af372e051c6f9b6139cfe734cd5ef29aa4a
st20326560 <st20326560@S01105>
2025-10-29 15:47:10 +0000

    Added ship object

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -18,7 +18,10 @@ int main(void) {
 	//
 	// Setup game scene objects here
 	//
-	
+	addObject("Player", glm::vec2(1,1),0,glm::vec2(0.5,0.5), "resources\\textures\\player1_ship.png");
+
+
+
 
 	// Enter main loop - this handles update and render calls
 	engineMainLoop();
