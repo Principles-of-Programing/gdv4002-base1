a6521c511a4d6b8bce0e54b1b39168d54d23cffb
st20326560 <st20326560@S01105>
2025-11-06 15:36:07 +0000

    Added USS Reliant as main Ship and added Player_1 as enemy and completed animation of charaters moving on to the moving the ship

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -1,12 +1,13 @@
 #include "Engine.h"
 
 // Function prototypes
+void myUpdate(GLFWwindow* window, double tDelta);
 
 
 int main(void) {
 	const float pi = 3.14159265359f;
 	// Initialise the engine (create window, setup OpenGL backend)
-	int initResult = engineInit("GDV4002 - Applied Maths for Games", 1024, 1024);
+	int initResult = engineInit("David's Asteroid Game", 1024, 1024);
 
 	// If the engine initialisation failed report error and exit
 	if (initResult != 0) {
@@ -14,7 +15,7 @@ int main(void) {
 		printf("Cannot setup game window!!!\n");
 		return initResult; // exit if setup failed
 	}
-	printf("Connecting to Jumpgate Network... Failed Accessing Local Sensors... Online !Incoming Contacts! \n");
+	printf("System Loaded Accessing...\n");
 
 	hideAxisLines();
 
@@ -22,38 +23,33 @@ int main(void) {
 	// Setup game scene objects here
 	//
 	addObject("Player1", glm::vec2(0.0f, 0.0f), glm::radians(0.0f), glm::vec2(1.0f, 1.0f));
-	addObject("Player2", glm::vec2(0.0f, 0.0f), glm::radians(0.0f), glm::vec2(1.0f, 1.0f));
+	addObject("Enemy Ship", glm::vec2(0.0f, 0.0f), glm::radians(0.0f), glm::vec2(1.0f, 1.0f));
 
 	GameObject2D* Player1Object =
-	getObject("Player1");
+	getObject("Enemy Ship");
 	if (Player1Object != nullptr) {
 		Player1Object->position = glm::vec2(-1.0f, -1.0f);
 		Player1Object->orientation = glm::radians(45.0f);
 		Player1Object->size = glm::vec2(0.25f, 0.25f);
 		Player1Object->textureID = loadTexture("resources\\textures\\player1_ship.png");
 	}
-	GameObject2D* Player2Object =
-		getObject("Player2");
-	if (Player2Object != nullptr) {
-		Player2Object->position = glm::vec2(1.0f, 1.0f);
-		Player2Object->orientation = glm::radians(225.0f);
-		Player2Object->size = glm::vec2(0.25f, 0.25f);
-		Player2Object->textureID = loadTexture("resources\\textures\\USS Relaint.png");
+	GameObject2D* EnemyShipObject =
+		getObject("Player1");
+	if (EnemyShipObject != nullptr) {
+		EnemyShipObject->position = glm::vec2(1.0f, 1.0f);
+		EnemyShipObject->orientation = glm::radians(225.0f);
+		EnemyShipObject->size = glm::vec2(0.25f, 0.25f);
+		EnemyShipObject->textureID = loadTexture("resources\\textures\\USS Relaint.png");
 	}
-
-
-
-
 	
-
-
-
-
-
-
+	setUpdateFunction(myUpdate);
+	float anglesPerSecond = glm::radians(45.0f);
+	float playerVelocity = 2.0f; // units per second
 
 	// Enter main loop - this handles update and render calls
 	engineMainLoop();
+
+
 
 	// When we quit (close window for example), clean up engine resources
 	engineShutdown();
@@ -62,4 +58,9 @@ int main(void) {
 	return 0;
 }
 
+void myUpdate(GLFWwindow* window, double tDelta) {
+	float player1RotationSpeed = glm::radians(90.0f); // degrees per second
+	GameObject2D* player1 = getObject("Player1");
+	player1->orientation += player1RotationSpeed * tDelta;
+}
 
