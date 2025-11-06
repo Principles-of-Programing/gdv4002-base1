445b1b2d0e25843b206b0925d564f0b6f3c04d52
David Le Petit-Cunningham <davidlepetitcunningham@outlook.com>
2025-11-06 20:27:48 +0000

    Requiring Bug Fixes

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -2,6 +2,8 @@
 
 // Function prototypes
 void myUpdate(GLFWwindow* window, double tDelta);
+float enemyPhase[3] = { 0.0f, 0.0f, 0.0f };
+float enemyPhaseVelocity[3] = { glm::radians(90.f), glm::radians(90.f), glm::radians(90.f) };
 
 
 int main(void) {
@@ -22,10 +24,14 @@ int main(void) {
 	//
 	// Setup game scene objects here
 	//
-	addObject("Player1", glm::vec2(0.0f, 0.0f), glm::radians(0.0f), glm::vec2(1.0f, 1.0f));
-	addObject("Enemy Ship", glm::vec2(0.0f, 0.0f), glm::radians(0.0f), glm::vec2(1.0f, 1.0f));
+	addObject("Player1", glm::vec2(-1.5f, 0.0f), glm::radians(0.0f), glm::vec2(0.5f, 0.5f), "resources\\textures\\USS Relaint.png");
+	addObject("Enemy", glm::vec2(0.0f, 0.0f), glm::radians(0.0f), glm::vec2(0.5f, 0.5f), "resources\\textures\\player1_ship.png");
+	addObject("Enemy", glm::vec2(1.5f, 0.0f), glm::radians(0.0f), glm::vec2(0.5f, 0.5f), "resources\\textures\\player1_ship.png");
+	addObject("Enemy", glm::vec2(0.0f, 1.5f), glm::radians(0.0f), glm::vec2(0.5f, 0.5f), "resources\\textures\\player1_ship.png");
 
-	GameObject2D* Player1Object =
+
+
+	/*GameObject2D* Player1Object =
 	getObject("Enemy Ship");
 	if (Player1Object != nullptr) {
 		Player1Object->position = glm::vec2(-1.0f, -1.0f);
@@ -41,10 +47,16 @@ int main(void) {
 		EnemyShipObject->size = glm::vec2(0.25f, 0.25f);
 		EnemyShipObject->textureID = loadTexture("resources\\textures\\USS Relaint.png");
 	}
+	*/
+	// Variables to control object movement
 	
+	// Set the update function for the engine
 	setUpdateFunction(myUpdate);
 	float anglesPerSecond = glm::radians(45.0f);
 	float playerVelocity = 2.0f; // units per second
+	listObjectCounts();
+	
+
 
 	// Enter main loop - this handles update and render calls
 	engineMainLoop();
@@ -59,8 +71,10 @@ int main(void) {
 }
 
 void myUpdate(GLFWwindow* window, double tDelta) {
-	float player1RotationSpeed = glm::radians(90.0f); // degrees per second
-	GameObject2D* player1 = getObject("Player1");
-	player1->orientation += player1RotationSpeed * tDelta;
+	GameObjectCollection enemies = getObjectCollection("enemy");
+	enemies.objectArray[0]->position.y = sinf(enemyPhase[0]);
+	enemyPhase[0] += enemyPhaseVelocity[0] * (float)tDelta;
+
+
 }
 
