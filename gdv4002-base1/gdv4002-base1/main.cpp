333bd0e7058b929b6ee30d69ba0f222bfd30542c
st20326560 <st20326560@S01105>
2025-11-27 14:56:00 +0000

    added more keyboard controls

--- gdv4002-base1/gdv4002-base1/main.cpp
+++ gdv4002-base1/gdv4002-base1/main.cpp
@@ -1,4 +1,5 @@
 #include "Engine.h"
+#include "glPrint.h"
 
 // Function prototypes
 void myUpdate(GLFWwindow* window, double tDelta);
@@ -11,11 +12,12 @@ bool wKeyPressed = false;
 bool aKeyPressed = false;
 bool sKeyPressed = false;
 bool dKeyPressed = false;
+bool escapeKeyPressed = false;
 
 int main(void) {
 	const float pi = 3.14159265359f;
 	// Initialise the engine (create window, setup OpenGL backend)
-	int initResult = engineInit("David's Asteroid Game", 1024, 1024);
+	int initResult = engineInit("GDV4002 - Program", 1024, 1024);
 
 	// If the engine initialisation failed report error and exit
 	if (initResult != 0) {
@@ -31,29 +33,10 @@ int main(void) {
 	// Setup game scene objects here
 	//
 	addObject("Player1", glm::vec2(-1.5f, 2.0f), glm::radians(0.0f), glm::vec2(0.5f, 0.5f), "resources\\textures\\USS Relaint.png");
-	addObject("Enemy", glm::vec2(0.0f, 0.0f), glm::radians(0.0f), glm::vec2(0.5f, 0.5f), "resources\\textures\\player1_ship.png");
-	addObject("Enemy", glm::vec2(1.5f, 0.0f), glm::radians(0.0f), glm::vec2(0.5f, 0.5f), "resources\\textures\\player1_ship.png");
-	addObject("Enemy", glm::vec2(-1.5f, 0.0f), glm::radians(0.0f), glm::vec2(0.5f, 0.5f), "resources\\textures\\player1_ship.png");
+	addObject("Enemy", glm::vec2(0.0f, 0.0f), glm::radians(-90.0f), glm::vec2(0.5f, 0.5f), "resources\\textures\\BattleCruiser.png");
+	addObject("Enemy", glm::vec2(1.5f, 0.0f), glm::radians(-90.0f), glm::vec2(0.5f, 0.5f), "resources\\textures\\BattleCruiser.png");
+	addObject("Enemy", glm::vec2(-1.5f, 0.0f), glm::radians(-90.0f), glm::vec2(0.5f, 0.5f), "resources\\textures\\BattleCruiser.png");
 
-
-
-	/*GameObject2D* Player1Object =
-	getObject("Enemy Ship");
-	if (Player1Object != nullptr) {
-		Player1Object->position = glm::vec2(-1.0f, -1.0f);
-		Player1Object->orientation = glm::radians(45.0f);
-		Player1Object->size = glm::vec2(0.25f, 0.25f);
-		Player1Object->textureID = loadTexture("resources\\textures\\player1_ship.png");
-	}
-	GameObject2D* EnemyShipObject =
-		getObject("Player1");
-	if (EnemyShipObject != nullptr) {
-		EnemyShipObject->position = glm::vec2(1.0f, 1.0f);
-		EnemyShipObject->orientation = glm::radians(225.0f);
-		EnemyShipObject->size = glm::vec2(0.25f, 0.25f);
-		EnemyShipObject->textureID = loadTexture("resources\\textures\\USS Relaint.png");
-	}
-	*/
 	// Variables to control object movement
 	
 	// Set the update function for the engine
@@ -90,6 +73,11 @@ void myUpdate(GLFWwindow* window, double tDelta) {
 	}
 }
 
+void windowcontrols(GLFWwindow* window, double tDelta) {
+	if (escapeKeyPressed) {
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	}
+}
 void playerControls(GameObject2D* player, double tDelta) {
 	if (wKeyPressed) {
 		glm::vec2 forward = glm::vec2(cosf(player->orientation), sinf(player->orientation));
@@ -113,6 +101,10 @@ void myKeyboardHandler(GLFWwindow* window, int key, int scancode, int action, in
 	// check if the key is pressed 
 	if (action == GLFW_PRESS) {
 		switch (key) {
+		case GLFW_KEY_ESCAPE:
+			escapeKeyPressed = true;
+			break;
+		
 		case GLFW_KEY_W:
 			wKeyPressed = true;
 			break;
@@ -133,21 +125,19 @@ void myKeyboardHandler(GLFWwindow* window, int key, int scancode, int action, in
 		switch (key) {
 		case GLFW_KEY_W:
 			wKeyPressed = false;
-			std::cout << "W key released" << std::endl;
 			break;
 		case GLFW_KEY_A:
 			aKeyPressed = false;
-			std::cout << "A key released" << std::endl;
 			break;
 		case GLFW_KEY_S:
 			sKeyPressed = false;
-			std::cout << "S key released" << std::endl;
 			break;
 		case GLFW_KEY_D:
 			dKeyPressed = false;
-			std::cout << "D key released" << std::endl;
 			break;
-
+		case GLFW_KEY_ESCAPE:
+			escapeKeyPressed = false;
+			break;
 		}
 	}
 }
